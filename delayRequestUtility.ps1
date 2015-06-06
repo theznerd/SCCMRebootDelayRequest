a46101c33aae81f39ea20a92bf27dfdca05f7a80
theznerd <nathan@z-nerd.com>
2015-06-05 18:40:24 -0600

    Update delayRequestUtility.ps1

--- delayRequestUtility.ps1
+++ delayRequestUtility.ps1
@@ -10,6 +10,7 @@ function GenerateResponse($challengeItem,$length){
 ######### BEGIN USER CONFIGURABLE CODE HERE #########
 #####################################################
 #Setup Your Credentials
+#DON'T use the following key... or do what you want since it's plaintext anyways.
 $key = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32)
 $secureString = ""
 $userForTransfer = ""
